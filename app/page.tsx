12c758df0cc96fca4a997e3fad3e651e7d71a8f8
unknown <wellyntonspagnol10@gmail.com>
2024-10-28 12:40:10 -0300

    start home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,83 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { DollarSign, Package, ShoppingBag, Users } from "lucide-react";
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Sales
+              </CardTitle>
+              <DollarSign className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              Total sales
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$40.000.00</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Orders
+              </CardTitle>
+              <ShoppingBag className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              New Orders
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">136</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Clients
+              </CardTitle>
+              <Users className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              New Clients
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">327</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Products
+              </CardTitle>
+              <Package className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              New Products
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">327</p>
+          </CardContent>
+        </Card>
+      </section>
+
+    </main>
   );
 }
--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,12 +1,110 @@
+import { Home, LogOut, Package, PanelBottom, Settings2, ShoppingBag, Users } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { Sheet, SheetTrigger, SheetContent } from "../ui/sheet";
-import { Home, Package, PanelBottom, Settings2, ShoppingBag, Users } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 export function Sibebar() {
   return (
     <div className="flex w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r 
+      bg-background sm:flex flex-col ">
+        <nav className="flex flex-col items-center gap-4 px-2 py-5">
+          <TooltipProvider>
+            <Link
+              href="#"
+              className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary
+              text-primary-foreground rounded-full">
+              <Package className="w-4 h-4" />
+              <span className="sr-only">Dashboard Avatar</span>
+            </Link>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg 
+                  text-muted-foreground transition-colors hover:text-foreground">
+                  <Home className="w-4 h-4" />
+                  <span className="sr-only">Home</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Home</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg 
+                  text-muted-foreground transition-colors hover:text-foreground">
+                  <ShoppingBag className="w-4 h-4" />
+                  <span className="sr-only">Orders</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Orders</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg 
+                  text-muted-foreground transition-colors hover:text-foreground">
+                  <Package className="w-4 h-4" />
+                  <span className="sr-only">Products</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Products</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg 
+                  text-muted-foreground transition-colors hover:text-foreground">
+                  <Users className="w-4 h-4" />
+                  <span className="sr-only">Clients</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Clients</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg 
+                  text-muted-foreground transition-colors hover:text-foreground">
+                  <Settings2 className="w-4 h-4" />
+                  <span className="sr-only">Settings</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Settings</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+
+        <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg 
+                  text-muted-foreground transition-colors hover:text-foreground">
+                  <LogOut className="w-4 h-4 text-red-500"/>
+                  <span className="sr-only">Settings</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">LogOut</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+      </aside>
+
+      <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header
           className="sticky top-0 z-30 flex h-14 items-center px-4 border-b 
           backgorund gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent 
@@ -77,9 +175,20 @@ export function Sibebar() {
                   Settings
                 </Link>
               </nav>
+
+              <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+                <Link
+                  href="#"
+                  className="flex items-center gap-4 px-2.5 text-muted-foreground
+                  hover:text-foreground"
+                  prefetch={false}>
+                  <LogOut className="w-5 h-5 transition-all" />
+                  logOut
+                </Link>
+              </nav>
             </SheetContent>
+
           </Sheet>
-          <h2>Menu</h2>
         </header>
       </div>
     </div>
